c5c029a0048c06344a06795ef681c7a3c6a7f5d1
udeani <udeani.henry@gmail.com>
2022-08-28 14:14:37 +0100

    Changes made:
    Implemented the Restful Endpoint
    Imported the filter image function

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import {Router, Request, Response} from 'express';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 (async () => {
@@ -32,6 +33,22 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //! END @TODO1
   
   // Root Endpoint
+app.get('/filteredimage', async(req: Request, res: Response) => {
+
+  const image_url = req.query.image_url.toString();
+  
+  if (!image_url) {
+    res.status(400).send('This service requires image url');
+  }
+
+  const filtered_image = await filterImageFromURL(image_url);
+
+  res.status(200).sendFile(filtered_image, () => {
+    deleteLocalFiles([filtered_image]);
+  });
+
+});
+
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
